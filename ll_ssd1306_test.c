4a61bd3cb3ca132c364295be88e370606ef75f0c
Viktor Sokolov <gzigzigzeo@gmail.com>
2020-04-22 00:23:14 +0400

    Added LICENSE and ll/LL prefix

--- ll_ssd1306_test.c
+++ ll_ssd1306_test.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 void assert_page_select_works() {
-  ssd1306_buffer_t buf = ssd1306_set_page_cmd(1);
-  assert(buf.buffer[2] == SSD1306_CMD_SET_PAM_PAGE_START_ADDRESS(1));
+  ll_ssd1306_buffer_t buf = ll_ssd1306_set_page_cmd(1);
+  assert(buf.buffer[2] == LL_SSD1306_CMD_SET_PAM_PAGE_START_ADDRESS(1));
   printf("assert_page_select()");
 }
 
