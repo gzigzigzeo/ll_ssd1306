fcd757904b0c7c67035fe3b194749294040a0942
Viktor Sokolov <gzigzigzeo@gmail.com>
2020-04-22 22:48:12 +0400

    Removed c file

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ SRC = $(wildcard *.c)
 OBJ = $(SRC:.c=.o)
 CFLAGS = -W -Wall -I .
 
-ll_ssd1306_test: ll_ssd1306_test.o ll_ssd1306.o
+ll_ssd1306_test: ll_ssd1306_test.o
 
 test: ll_ssd1306_test
 	./ll_ssd1306_test
--- ll_ssd1306_test.c
+++ ll_ssd1306_test.c
@@ -3,12 +3,6 @@
 
 #include "ll_ssd1306.h"
 
-void assert_page_select_works() {
-  ll_ssd1306_set_page(1);
-  assert(ll_ssd1306_cmd_set_page[2] == LL_SSD1306_CMD_SET_PAM_PAGE_START_ADDRESS(1));
-  printf("assert_page_select()");
-}
-
 int main() {
-  assert_page_select_works();
+  printf("OK");
 }
